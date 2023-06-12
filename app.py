1d95502a6ba020f245e56a2e042e2ccfa53365d3
Amanda Souza <amandasilvadsz@gmail.com>
2023-06-11 22:01:22 -0300

    On branch main 	modified:   app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-from flask import Flask, render_template, redirect, url_for, request, jsonify
+from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
 
 topiks = []
-
 
 @app.route('/')
 def index():
@@ -13,19 +12,21 @@ def index():
 @app.route('/adicionar-topik', methods=['GET', 'POST'])
 def adicionar_topik():
   if request.method == 'POST':
-   horario = request.form['horario']
-   motorista = request.form['motorista']
-   destino = request.form['destino']
-   topik = {'id': len(topiks), 'motorista': motorista, 'destino': destino, 'horario': horario}
-   topiks.append(topik)
-   return redirect('listar-topiks')
+      horario = request.form['horario']
+      motorista = request.form['motorista']
+      destino = request.form['destino']
+      topik = {'id': len(topiks), 'motorista': motorista, 'destino': destino, 'horario': horario}
+      topiks.append(topik)
+      return redirect('listar-topiks')
   else:
-    return render_template('paginas/adicionar.html')
+      return render_template('paginas/adicionar.html')
 
+#Rota para Listar as Topiks
 @app.route('/listar-topiks')
 def listar_topiks():
     return render_template('paginas/listar.html', topiks=topiks)
   
+#Rota para Editar uma Topik
 @app.route('/editar-topik/<int:id>', methods=['GET', 'POST'])
 def editar_topik(id):
     topik = next((topik for topik in topiks if topik['id'] == id), None)
@@ -39,6 +40,7 @@ def editar_topik(id):
         return render_template('paginas/editar.html', topik=topik)
     return "Topik não encontrada." 
   
+#Rota para Excluir uma Topik
 @app.route('/excluir-topik/<int:id>')
 def excluir_topik(id):
     del topiks[id]
